315f4eb02cdd59e18251990efa235f31b682e172
Ponito <vvbdac58@gmail.com>
2025-12-24 22:31:56 +0800

    lint...

--- Lilhelper/Objs/FluentExts.cs
+++ Lilhelper/Objs/FluentExts.cs
@@ -1,16 +1,12 @@
 using System;
 
-namespace Lilhelper.Objs
-{
-    public static class FluentExts
-    {
-        public static T Let<T>(this T t, Func<T, T> func)
-        {
+namespace Lilhelper.Objs {
+    public static class FluentExts {
+        public static T Let<T>(this T t, Func<T, T> func) {
             return func(t);
         }
 
-        public static U So<T, U>(this T t, Func<T, U> func)
-        {
+        public static U So<T, U>(this T t, Func<T, U> func) {
             return func(t);
         }
     }
